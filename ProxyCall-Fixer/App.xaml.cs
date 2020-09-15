8e238acaf3721bccb1c79b202a7acf6c0e6bf581
Timur Yakubov <strangepruss@gmail.com>
2020-09-15 12:02:36 +0300

    Added IlRepack

--- ProxyCall-Fixer/App.xaml.cs
+++ ProxyCall-Fixer/App.xaml.cs
@@ -13,5 +13,9 @@ namespace ProxyCall_Remover
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            new MainWindow().ShowDialog();
+        }
     }
 }
